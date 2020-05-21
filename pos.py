1c94e02b452a196618568f1c5c080761a5c7b4db
ZacharyLaw <48851717+ZacharyLaw@users.noreply.github.com>
2020-05-21 15:14:16 +0800

    Update pos.py

--- pos.py
+++ pos.py
@@ -3,24 +3,46 @@ import pandas as pd
 import msvcrt
 from datetime import datetime
 from os import system, name 
+import sys
+try:inventory= pd.read_csv('inventory.csv')
+except FileNotFoundError:
+    print('Error: Inventory File not found')
+    sys.exit()
 cart=[]
 cred=[]
-inventory= pd.read_csv('inventory.csv')#, index_col=0
+y=0
+tab=0
+swap={1:0,0:1}
+number_of_items=len(inventory['Name'])
 _ = system('cls') 
-print('Enter number for item:\n'+inventory[['Index','Name']].to_string(index=False,header=False))
-dict={b'1':0,b'2':1,b'3':2,b'4':3,b'5':4,b'6':5,b'7':6,b'8':7,b'9':8}
+print('Inventory:')
+for row in range(number_of_items):
+    if row==y:print('%-10s    $%3s'%('<'+inventory.loc[row,'Name']+'>',inventory.loc[row,'Price']))
+    else:print('%-10s    $%3s'%(' '+inventory.loc[row,'Name']+' ',inventory.loc[row,'Price']))
+print('\n\n↑ ↓ Select Item        Space Bar To Select         Tab Switch to Shopping Cart        Esc To Exit')
 while True:
     choice=msvcrt.getch()
     _ = system('cls') 
-    if choice in [b'1',b'2',b'3',b'4',b'5',b'6',b'7',b'8',b'9']:
-        cart.append(inventory.loc[dict[choice],'Name'])
-        cred.append(int(inventory.loc[dict[choice],'Price']))
-        print('Enter number for item:\n'+inventory[['Index','Name']].to_string(index=False,header=False))    
-        print('Shopping cart: '+str(cart).strip('[]').replace("'",""))
-        print('Price: '+str(sum(cred)))
-    elif choice == b'\r':
-        print('              121 Supermarket\n      83 Tat Chee Ave, Kowloon Tong\n        New Territories, Hong Kong\nCasher: Self    '+str(datetime.now()))
-        for x in range(len(cart)):print('   %-30s %d'%(cart[x],cred[x]))
-        print('   Total:                         $'+str(sum(cred)))
+    if choice==b'H' and y!=0:y-=1#^
+    elif choice==b'P' and y!=number_of_items-1:y+=1#V
+    elif choice==b'\t':tab=swap[tab]
+    elif choice==b' ':
+        cart.append(inventory.loc[y,'Name'])
+        cred.append(int(inventory.loc[y,'Price']))
+    elif choice == b'\r' and len(cart)!=0:
+        print('         121 Supermarket\n  83 Tat Chee Ave, Kowloon Tong\n   New Territories, Hong Kong\nCasher: Self    '+str(datetime.now().strftime('%Y/%m/%d %H:%M')))
+        for x in range(len(cart)):print('   %-20s %5d'%(cart[x],cred[x]))
+        print('   Total:                 $'+str(sum(cred)))
         break
-    elif choice ==b'q':break
+    elif choice ==b'q' or choice == b"\x1b":break
+    print('Inventory:')
+    for row in range(number_of_items):
+        if row==y and tab==0:print('%-20s    $%3s'%('<'+inventory.loc[row,'Name']+'>',inventory.loc[row,'Price']))
+        else:print('%-20s    $%3s'%(' '+inventory.loc[row,'Name']+' ',inventory.loc[row,'Price']))
+    if len(cart)==0:print('\n\n↑ ↓ Select Item        Space Bar To Select          Tab Switch to Shopping Cart        Esc To Exit')
+    else:
+        print('\n\n%-18sTotal:$%3d'%('Shopping Cart:',sum(cred)))
+        for row in range(len(cart)):
+            if row==y and tab==1:print('%-20s    $%3s'%('<'+cart[row]+'>',cred[row]))
+            else:print('%-20s    $%3s'%(' '+cart[row]+' ',cred[row]))
+        print('\n\n↑ ↓ Select Item        Space Bar To Unselect        Tab Switch to Shopping Cart        Esc To Exit       Enter To Check Out')
